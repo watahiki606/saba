fb78d8daa55f71e00b6091228d8b1fdda5ee7d37
watahiki606 <v10acdict@gmail.com>
2024-11-24 17:06:45 +0900

    Url構造体のコンストラクタを作成

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,10 +1,22 @@
-use alloc::string::String;
+use alloc::string::{String, ToString};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
     url: String,
     host: String,
-    port: u16,
+    port: String,
     path: String,
     searchpart: String,
+}
+
+impl Url {
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            host: "".to_string(),
+            port: "".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        }
+    }
 }
