d9001d87ed9f6a2b17c336029a9709d969d2bd08
watahiki606 <v10acdict@gmail.com>
2024-11-27 08:20:26 +0900

    URLには必ずhttp://をつけることにする

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -21,6 +21,16 @@ impl Url {
     }
 
     pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP schema is supported.".to_string());
+        }
         // TODO: Implement the parsing logic
+    }
+
+    fn is_http(&self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
     }
 }
