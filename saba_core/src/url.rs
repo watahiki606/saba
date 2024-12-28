7f15831b822c80b6f2a491dfdc9bbb1e4555d609
watahiki606 <v10acdict@gmail.com>
2024-12-28 13:09:24 +0900

    ゲッターを追加

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -101,4 +101,20 @@ impl Url {
             path_and_serchpart[1].to_string()
         }
     }
+
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+
+    pub fn port(&self) -> String {
+        self.port.clone()
+    }
+
+    pub fn path(&self) -> String {
+        self.path.clone()
+    }
+
+    pub fn searchpart(&self) -> String {
+        self.searchpart.clone()
+    }
 }
