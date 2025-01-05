b833fb8e8e5af0ad990f2d52a6ce69bd3089ca5c
watahiki606 <v10acdict@gmail.com>
2025-01-05 16:17:52 +0900

    Add HTTP GET method stub to HttpClient for future implementation

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -4,4 +4,8 @@ impl HttpClient {
     pub fn new() -> Self {
         Self {}
     }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        // TODO: Implement HTTP GET request
+    }
 }
