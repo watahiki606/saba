053ee53ce827b1d3122a639a77954fe3d78dace7
watahiki606 <v10acdict@gmail.com>
2025-01-13 17:42:26 +0900

    ヘッダの構築

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -42,5 +42,11 @@ impl HttpClient {
         let mut request = String::from("GET /");
         request.push_str(&path);
         request.push_str(" HTTP/1.1\n");
+        request.push_str("Host: ");
+        request.push_str(&host);
+        request.push_str("\n");
+        request.push_str("Accept: text/html\n");
+        request.push_str("Connection: close\n");
+        request.push_str("\n");
     }
 }
