40b25f1fdb765cfd1351bf098c96850a41f025dc
watahiki606 <v10acdict@gmail.com>
2025-01-13 17:47:08 +0900

    リクエストの送信

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -49,5 +49,16 @@ impl HttpClient {
         request.push_str("Accept: text/html\n");
         request.push_str("Connection: close\n");
         request.push_str("\n");
+
+        let _byte_written = match stream.write(request.as_bytes()) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to send a request to TCP stream".to_string(),
+                ));
+            }
+        };
+
+        
     }
 }
