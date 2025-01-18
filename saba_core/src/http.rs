e031cf1049880df7b3ad572eea19b049d050cc10
watahiki606 <v10acdict@gmail.com>
2025-01-18 11:41:47 +0900

    文字列の前処理

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -10,6 +10,13 @@ pub struct HttpResponse {
     body: String,
 }
 
+impl HttpResponse {
+    pub fn new(raw_response: String) -> Result<Self, Error> {
+        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+    }
+}
+
+
 #[derive(Debug, Clone)]
 pub struct Header {
     name: String,
