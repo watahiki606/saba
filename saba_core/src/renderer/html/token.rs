7254208ef9de97f1c683b92f814313da3743272e
watahiki606 <v10acdict@gmail.com>
2025-02-20 09:21:36 +0900

    HtmlToken 列挙型の作成

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -1,3 +1,4 @@
+use crate::renderer::html::attribute::Attribute;
 use alloc::string::String;
 use alloc::vec::Vec;
 
@@ -22,4 +23,22 @@ impl HtmlTokenizer {
             buf: String::new(),
         }
     }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    // 開始タグ
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    // 終了タグ
+    EndTag {
+        tag: String,
+    },
+    // 文字
+    Char(char),
+    // ファイルの終了(End Of File)
+    Eof,
 }
