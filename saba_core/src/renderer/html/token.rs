956f0ccb1a3ffbcfb67ff5e65901f5ffb713537d
watahiki606 <v10acdict@gmail.com>
2025-03-31 08:34:21 +0900

    HtmlTokenizer にシングルクオートで囲まれた属性値の処理を追加し、状態遷移を強化

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -294,6 +294,21 @@ impl Iterator for HtmlTokenizer {
                     self.append_attribute(c, /*is_name*/ false);
                 }
 
+                State::AttributeValueSingleQuoted => {
+                    if c == '\'' {
+                        self.state = State::AfterAttributeValueQuoted;
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    self.append_attribute(c, /*is_name*/ false);
+                }
+                
+                
+
                 
 
                     
