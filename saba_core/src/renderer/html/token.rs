d3c3c553150bab0e5c80a8d438d8bc0a02b695e1
watahiki606 <v10acdict@gmail.com>
2025-03-31 09:10:47 +0900

    HtmlTokenizer にスクリプトデータ終了タグの処理を追加。スラッシュ検出時に状態を再設定し、適切なトークンを返すロジックを実装。

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -403,6 +403,22 @@ impl Iterator for HtmlTokenizer {
                     self.state = State::ScriptData;
                     return Some(HtmlToken::Char('<'));
                 }
+
+                State::ScriptDataEndTagOpen => {
+                    if c.is_ascii_alphabetic() {
+                        self.reconsume = true;
+                        self.state = State::ScriptDataEndTagName;
+                        self.create_tag(false);
+                        continue;
+                    }
+
+                    self.reconsume = true;
+                    self.state = State::ScriptData;
+                    // 仕様では、"<" と "/"の２つの文字トークンを返すとなっているが、
+                    // 私達の実装ではnextメソッドからは一つのトークンしか返せないため、
+                    // ここでは"<" のトークンのみを返す
+                    return Some(HtmlToken::Char('<'));
+                }
             }
         }
     }
