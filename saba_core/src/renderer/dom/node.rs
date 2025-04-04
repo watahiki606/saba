b881839c88e887a3b1b9b95a8fa786ac7d88db44
watahiki606 <v10acdict@gmail.com>
2025-04-04 09:16:44 +0900

    DOMモジュールにNode構造体を追加。ノードの種類、親子関係、兄弟関係を管理するためのフィールドを定義し、新しいノードを生成するためのコンストラクタを実装。

---
+++ saba_core/src/renderer/dom/node.rs
@@ -0,0 +1,28 @@
+use alloc::rc::Rc;
+use alloc::rc::Weak;
+use core::cell::RefCell;
+
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub kind: NodeKind,
+    window: Weak<RefCell<Window>>,
+    parent: Weak<RefCell<Node>>,
+    first_child: Option<Rc<RefCell<Node>>>,
+    last_child: Weak<RefCell<Node>>,
+    previous_sibling: Weak<RefCell<Node>>,
+    next_sibling: Option<Rc<RefCell<Node>>>,
+}
+
+impl Node {
+    pub fn new(kind: NodeKind) -> Self {
+        Self {
+            kind,
+            window: Weak::new(),
+            parent: Weak::new(),
+            first_child: None,
+            last_child: Weak::new(),
+            previous_sibling: Weak::new(),
+            next_sibling: None,
+        }
+    }
+}
