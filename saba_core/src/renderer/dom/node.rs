dd1fdee7950120bf4e9db7203ab10cd5e393aa2d
watahiki606 <v10acdict@gmail.com>
2025-04-06 18:07:01 +0900

    Node構造体に新たにノードの種類を取得するメソッドを追加。具体的には、ノードの種類を返す`kind`メソッド、要素を取得する`get_element`メソッド、要素の種類を取得する`element_kind`メソッドを実装。

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -101,6 +101,24 @@ impl Node {
     pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
         self.window = window;
     }
+
+    pub fn kind(&self) -> NodeKind {
+        self.kind.clone()
+    }
+
+    pub fn get_element(&self) -> Option<Element> {
+        match self.kind {
+            NodeKind::Document | NodeKind::Text(_) => None,
+            NodeKind::Element(ref e) => Some(e.clone()),
+        }
+    }
+
+    pub fn element_kind(&self) -> Option<ElementKind> {
+        match self.kind {
+            NodeKind::Document | NodeKind::Text(_) => None,
+            NodeKind::Element(ref e) => Some(e.kind),
+        }
+    }
 }
 
 impl Window {
