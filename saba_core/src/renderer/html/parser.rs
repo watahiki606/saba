949cb8f8603417cacc67bd7b398e2908296492c9
watahiki606 <v10acdict@gmail.com>
2025-04-12 15:21:45 +0900

    HtmlParserの`AfterBody`挿入モードを実装し、`html`タグの終了処理を追加。EOFトークンや文字トークンの処理を強化し、挿入モードの遷移を改善。

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -240,6 +240,26 @@ impl HtmlParser {
                     }
                     self.mode = self.original_insertion_mode;
                 }
+                InsertionMode::AfterBody => {
+                    match token {
+                        Some(HtmlToken::Char(_c)) => {
+                            token = self.t.next();
+                            continue;
+                        }
+                        Some(HtmlToken::EndTag { ref tag }) => {
+                            if tag == "html" {
+                                self.mode = InsertionMode::AfterAfterBody;
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::Eof) | None => {
+                            return self.window.clone();
+                        }
+                        _ => {}
+                    }
+                    self.mode = InsertionMode::InBody;
+                }
             }
         }
         self.window.clone()
