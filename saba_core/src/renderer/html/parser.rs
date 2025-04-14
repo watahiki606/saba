fbcec363b28161ebb308294341277d58a43bb16b
watahiki606 <v10acdict@gmail.com>
2025-04-15 08:17:10 +0900

    HtmlParserに新しい`pop_until`および`contain_in_stack`メソッドを追加し、スタック内の指定された`ElementKind`の要素を管理するロジックを実装。これにより、スタック操作の効率が向上し、要素の存在確認が容易になる。

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -345,6 +345,35 @@ impl HtmlParser {
 
         false
     }
+
+    fn pop_until(&mut self, element_kind: ElementKind) {
+        assert!(
+            self.contain_in_stack(element_kind),
+            "stack doesn't have an element {:?}",
+            element_kind
+        );
+
+        loop {
+            let current = match self.stack_of_open_elements.pop() {
+                Some(n) => n,
+                None => return,
+            };
+
+            if current.borrow().element_kind() == Some(element_kind) {
+                return;
+            }
+        }
+    }
+
+    fn contain_in_stack(&mut self, element_kind: ElementKind) -> bool {
+        for i in 0..self.stack_of_open_elements.len() {
+            if self.stack_of_open_elements[i].borrow().element_kind() == Some(element_kind) {
+                return true;
+            }
+        }
+
+        false
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
