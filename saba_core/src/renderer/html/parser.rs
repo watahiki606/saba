00e2eb52f0c0ac159de12aefe8a0606e6b9fc512
watahiki606 <v10acdict@gmail.com>
2025-04-18 08:27:36 +0900

    HtmlParserに新しい`insert_char`メソッドを追加し、文字をノードとして挿入するロジックを実装。テキストノードへの文字追加や、改行・空白文字の処理を強化し、DOMツリーの構築をさらに改善。

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -381,6 +381,48 @@ impl HtmlParser {
         s.push(c);
         Node::new(NodeKind::Text(s))
     }
+
+    fn insert_char(&mut self, c: char) {
+        let current = match self.stack_of_open_elements.last() {
+            Some(n) => n.clone(),
+            None => return,
+        };
+
+        // 現在参照しているノードがテキストノードの場合、そのノードに文字を追加する
+        if let NodeKind::Text(ref mut s) = current.borrow().kind {
+            s.push(c);
+            return;
+        }
+
+        // 改行文字や空白文字のときはテキストノードを追加しない
+        if c == '\n' || c == ' ' {
+            return;
+        }
+
+        let node = Rc::new(RefCell::new(self.create_char(c)));
+
+        if current.borrow().first_child().is_some() {
+            current
+                .borrow()
+                .first_child()
+                .unwrap()
+                .borrow_mut()
+                .set_next_sibling(Some(node.clone()));
+            node.borrow_mut().set_previous_sibling(Rc::downgrade(
+                &current
+                    .borrow()
+                    .first_child()
+                    .expect("failed to get a first child"),
+            ))
+        } else {
+            current.borrow_mut().set_first_child(Some(node.clone()));
+        }
+
+        current.borrow_mut().set_last_child(Rc::downgrade(&node));
+        node.borrow_mut().set_parent(Rc::downgrade(&current));
+
+        self.stack_of_open_elements.push(node);
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
