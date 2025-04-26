818717dcaebc664ea5255f1bed0f9056ee6d3979
watahiki606 <v10acdict@gmail.com>
2025-04-26 17:15:47 +0900

    HttpResponseからDOMツリーを作成

--- saba_core/src/renderer/page.rs
+++ saba_core/src/renderer/page.rs
@@ -1,7 +1,13 @@
+use crate::alloc::string::ToString;
 use crate::browser::Browser;
+use crate::http::HttpResponse;
 use crate::renderer::dom::node::Window;
+use crate::renderer::html::parser::HtmlParser;
+use crate::renderer::html::token::HtmlTokenizer;
+use crate::utils::convert_dom_to_string;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
+use alloc::string::String;
 use core::cell::RefCell;
 
 #[derive(Debug, Clone)]
@@ -20,5 +26,24 @@ impl Page {
 
     pub fn set_browser(&mut self, browser: Weak<RefCell<Browser>>) {
         self.browser = browser;
+    }
+
+    pub fn receive_response(&mut self, response: HttpResponse) -> String {
+        self.create_frame(response.body());
+
+        // デバッグ用にDOMツリーを文字列として返す
+        if let Some(frame) = &self.frame {
+            let dom = frame.borrow().document().clone();
+            let debug = convert_dom_to_string(&Some(dom));
+            return debug;
+        }
+
+        "".to_string()
+    }
+
+    pub fn create_frame(&mut self, html: String) {
+        let html_tokenizer = HtmlTokenizer::new(html);
+        let frame = HtmlParser::new(html_tokenizer).construct_tree();
+        self.frame = Some(frame);
     }
 }
