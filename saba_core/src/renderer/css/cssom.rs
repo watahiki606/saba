b4f7b03598fc85870634f2265b23dd93811fab91
watahiki606 <v10acdict@gmail.com>
2025-05-03 10:31:24 +0900

    cssParser構造体の作成

---
+++ saba_core/src/renderer/css/cssom.rs
@@ -0,0 +1,13 @@
+use crate::renderer::css::token::CssTokenizer;
+use core::iter::Peekable;
+
+#[derive(Debug, Clone)]
+pub struct CssParser {
+    t: Peekable<CssTokenizer>,
+}
+
+impl CssParser {
+    pub fn new(t: CssTokenizer) -> Self {
+        Self { t: t.peekable() }
+    }
+}
