64602ad20a73fee46ebc53cb7724aab3e89cff17
watahiki606 <v10acdict@gmail.com>
2025-05-03 10:45:28 +0900

    ルートノード(StyleSheet)の作成

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -1,5 +1,21 @@
 use crate::renderer::css::token::CssTokenizer;
+use alloc::vec::Vec;
 use core::iter::Peekable;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StyleSheet {
+    pub rules: Vec<QualifiedRule>,
+}
+
+impl StyleSheet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn set_rules(&mut self, rules: Vec<QualifiedRule>) {
+        self.rules = rules;
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct CssParser {
