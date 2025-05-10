4881dae8550852713ac744c327a78b06a41d0332
watahiki606 <v10acdict@gmail.com>
2025-05-10 14:38:21 +0900

    ComputedStyleにCSSプロパティの設定および取得メソッドを追加

--- saba_core/src/renderer/layout/computed_style.rs
+++ saba_core/src/renderer/layout/computed_style.rs
@@ -21,4 +21,59 @@ impl ComputedStyle {
             width: None,
         }
     }
+
+    pub fn set_background_color(&mut self, color: Color) {
+        self.background_color = Some(color);
+    }
+
+    pub fn background_color(&self) -> Color {
+        self.background_color
+            .clone()
+            .expect("failed to access CSS property: background-color")
+    }
+
+    pub fn set_color(&mut self, color: Color) {
+        self.color = Some(color);
+    }
+
+    pub fn color(&self) -> Color {
+        self.color
+            .clone()
+            .expect("failed to access CSS property: color")
+    }
+
+    pub fn set_display(&mut self, display: DisplayType) {
+        self.display = Some(display);
+    }
+
+    pub fn display(&self) -> DisplayType {
+        self.display
+            .expect("failed to access CSS property: display")
+    }
+
+    pub fn font_size(&self) -> FontSize {
+        self.font_size
+            .expect("failed to access CSS property: font_size")
+    }
+
+    pub fn text_decoration(&self) -> TextDecoration {
+        self.text_decoration
+            .expect("failed to access CSS property: text_decoration")
+    }
+
+    pub fn set_height(&mut self, height: f64) {
+        self.height = Some(height);
+    }
+
+    pub fn height(&self) -> f64 {
+        self.height.expect("failed to access CSS property: height")
+    }
+
+    pub fn set_width(&mut self, width: f64) {
+        self.width = Some(width);
+    }
+
+    pub fn width(&self) -> f64 {
+        self.width.expect("failed to access CSS property: width")
+    }
 }
