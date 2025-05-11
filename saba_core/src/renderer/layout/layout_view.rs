e3df2b756d6637db00ff486ea771e6718d086d6b
watahiki606 <v10acdict@gmail.com>
2025-05-12 08:20:58 +0900

    レイアウトツリーの作成

--- saba_core/src/renderer/layout/layout_view.rs
+++ saba_core/src/renderer/layout/layout_view.rs
@@ -2,6 +2,7 @@ use crate::renderer::css::cssom::StyleSheet;
 use crate::renderer::dom::api::get_target_element_node;
 use crate::renderer::dom::node::ElementKind;
 use crate::renderer::dom::node::Node;
+use crate::renderer::layout::layout_object::create_layout_object;
 use crate::renderer::layout::layout_object::LayoutObject;
 use alloc::rc::Rc;
 use core::cell::RefCell;
@@ -29,4 +30,81 @@ impl LayoutView {
     pub fn root(&self) -> Option<Rc<RefCell<LayoutObject>>> {
         self.root.clone()
     }
+}
+
+fn build_layout_tree(
+    node: &Option<Rc<RefCell<Node>>>,
+    parent_obj: &Option<Rc<RefCell<LayoutObject>>>,
+    cssom: &StyleSheet,
+) -> Option<Rc<RefCell<LayoutObject>>> {
+    let mut target_node = node.clone();
+    let mut layout_object = create_layout_object(node, parent_obj, cssom);
+
+    while layout_object.is_none() {
+        if let Some(n) = target_node {
+            target_node = n.borrow().next_sibling().clone();
+            layout_object = create_layout_object(&target_node, parent_obj, cssom);
+        } else {
+            return layout_object;
+        }
+    }
+
+    if let Some(n) = target_node {
+        let original_first_child = n.borrow().first_child();
+        let original_next_sibling = n.borrow().next_sibling();
+        let mut first_child = build_layout_tree(&original_first_child, &layout_object, cssom);
+        let mut next_sibling = build_layout_tree(&original_next_sibling, &None, cssom);
+
+        if first_child.is_none() && original_first_child.is_some() {
+            let mut original_dom_node = original_first_child
+                .expect("first child should exist")
+                .borrow()
+                .next_sibling();
+
+            loop {
+                first_child = build_layout_tree(&original_dom_node, &layout_object, cssom);
+
+                if first_child.is_none() && original_dom_node.is_some() {
+                    original_dom_node = original_dom_node
+                        .expect("next sibling should exist")
+                        .borrow()
+                        .next_sibling();
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        if next_sibling.is_none() && n.borrow().next_sibling().is_some() {
+            let mut original_dom_node = original_next_sibling
+                .expect("first child should exist")
+                .borrow()
+                .next_sibling();
+
+            loop {
+                next_sibling = build_layout_tree(&original_dom_node, &None, cssom);
+
+                if next_sibling.is_none() && original_dom_node.is_some() {
+                    original_dom_node = original_dom_node
+                        .expect("next sibling should exist")
+                        .borrow()
+                        .next_sibling();
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        let obj = match layout_object {
+            Some(ref obj) => obj,
+            None => panic!("render object should exist here"),
+        };
+
+        obj.borrow_mut().set_first_child(first_child);
+        obj.borrow_mut().set_next_sibling(next_sibling);
+    }
+
+    layout_object
 }
