5f8deafc5f3da653471aa15f7ef2f0709c0ba954
watahiki606 <v10acdict@gmail.com>
2025-05-26 09:05:30 +0900

    画面に描画するために使用するDisplayitem列挙体を作成

---
+++ saba_core/src/display_item.rs
@@ -0,0 +1,19 @@
+use crate::renderer::layout::computed_style::ComputedStyle;
+use crate::renderer::layout::layout_object::LayoutPoint;
+use crate::renderer::layout::layout_object::LayoutSize;
+use alloc::string::String;
+
+#[derive(Debug, Clone, PartialEq)]
+/// - 本書のブラウザでは資格とテキストを描画できる
+pub enum Displayitem {
+    Rect {
+        stle: ComputedStyle,
+        layout_point: LayoutPoint,
+        layout_size: LayoutSize,
+    },
+    Text {
+        text: String,
+        style: ComputedStyle,
+        layout_point: LayoutPoint,
+    },
+}
