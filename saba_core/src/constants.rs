7aa3b4911e2e7e351508143202a19d02faef59fa
watahiki606 <v10acdict@gmail.com>
2025-05-30 07:52:11 +0900

    背景の白い資格を描画する

--- saba_core/src/constants.rs
+++ saba_core/src/constants.rs
@@ -1,3 +1,8 @@
+pub static WHITE: u32 = 0xffffffff;
+
+pub static WINDOW_INIT_X_POS: i64 = 30;
+pub static WINDOW_INIT_Y_POS: i64 = 50;
+
 pub static WINDOW_WIDTH: i64 = 600;
 pub static WINDOW_HEIGHT: i64 = 400;
 pub static WINDOW_PADDING: i64 = 5;
---
+++ ui/wasabi/src/app.rs
@@ -0,0 +1,33 @@
+use crate::alloc::string::ToString;
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::window::Window;
+use saba_core::browser::Browser;
+use saba_core::constants::WHITE;
+use saba_core::constants::WINDOW_HEIGHT;
+use saba_core::constants::WINDOW_INIT_X_POS;
+use saba_core::constants::WINDOW_INIT_Y_POS;
+use saba_core::constants::WINDOW_WIDTH;
+
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+}
