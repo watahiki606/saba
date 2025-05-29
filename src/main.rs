c7240b752295388d18a4044fde9d8522f37263a0
watahiki606 <v10acdict@gmail.com>
2025-05-30 08:44:42 +0900

    未使用コードを削除

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,11 @@
 
 extern crate alloc;
 
-use crate::alloc::string::ToString;
 use alloc::rc::Rc;
 use core::cell::RefCell;
-use net_wasabi::http::HttpClient;
 use noli::prelude::*;
-use noli::*;
 use saba_core::browser::Browser;
-use saba_core::http::HttpResponse;
 use ui_wasabi::app::WasabiUI;
-
-static TEST_HTTP_RESPONSE: &str = r#"
-HTTP/1.1 200 OK
-Data: xx xx xx
-
-<html>
-<head></head>
-<body>
-  <h1 id="title">H1 title</h1>
-  <h2 class="class">H2 title</h2>
-  <p>Test text.</p>
-  <p>
-  <a href= "example.com">Link1</a>
-  <a href= "example.com">Link2</a>
-  </p>
-</body>
-</html>
-"#;
 
 fn main() -> u64 {
     // Browser 構造体を初期化
