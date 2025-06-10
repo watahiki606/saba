025e80aa96e3c6e6c1b5539af7cfae5d3b6c5b1b
watahiki606 <v10acdict@gmail.com>
2025-06-11 07:31:13 +0900

    DOMツリーのノードの指定した属性の値を取得する

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -188,6 +188,15 @@ impl Element {
     pub fn attributes(&self) -> Vec<Attribute> {
         self.attributes.clone()
     }
+
+    pub fn get_attribute(&self, name: &str) -> Option<String> {
+        for attr in &self.attributes {
+            if attr.name() == name {
+                return Some(attr.value());
+            }
+        }
+        None
+    }
 }
 
 impl FromStr for ElementKind {
