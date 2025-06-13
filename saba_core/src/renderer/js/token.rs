b0e211bd147292af37bc5e346f5be6a5809f0472
watahiki606 <v10acdict@gmail.com>
2025-06-13 09:22:34 +0900

    記号トークンを返す

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -20,3 +20,35 @@ impl JsLexer {
         }
     }
 }
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // ホワイトスペースまたは改行文字が続く限り、次の位置に進める
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            _ => unimplemented!("char {:?} is not supported", c),
+        };
+
+        Some(token)
+    }
+}
