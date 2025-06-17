4715ef2ecf81a24c86e9d1b1cbed794b863dd1ff
watahiki606 <v10acdict@gmail.com>
2025-06-18 07:27:36 +0900

    ASTノードの列挙型を追加

---
+++ saba_core/src/renderer/js/ast.rs
@@ -0,0 +1,21 @@
+use alloc::rc::Rc;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Node {
+    ExpressionStatement(Option<Rc<Node>>),
+    AdditiveExpression {
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    },
+    AssignmentExpression {
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    },
+    MemberExpression {
+        object: Option<Rc<Node>>,
+        property: Option<Rc<Node>>,
+    },
+    NumericLiteral(u64),
+}
