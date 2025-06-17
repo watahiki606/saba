ed6f0070dda7b5e59281fc1a5bcb6d0c5b49d826
watahiki606 <v10acdict@gmail.com>
2025-06-18 08:15:39 +0900

    ASTノードに新しい式、加算、代入、メンバー、数値リテラルの生成メソッドを追加

--- saba_core/src/renderer/js/ast.rs
+++ saba_core/src/renderer/js/ast.rs
@@ -19,3 +19,44 @@ pub enum Node {
     },
     NumericLiteral(u64),
 }
+
+impl Node {
+    pub fn new_expression_statement(expression: Option<Rc<Self>>) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::ExpressionStatement(expression)))
+    }
+
+    pub fn new_additive_expression(
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    ) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::AdditiveExpression {
+            operator,
+            left,
+            right,
+        }))
+    }
+
+    pub fn new_assignment_expression(
+        operator: char,
+        left: Option<Rc<Node>>,
+        right: Option<Rc<Node>>,
+    ) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::AssignmentExpression {
+            operator,
+            left,
+            right,
+        }))
+    }
+
+    pub fn new_member_expression(
+        object: Option<Rc<Self>>,
+        property: Option<Rc<Self>>,
+    ) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::MemberExpression { object, property }))
+    }
+
+    pub fn new_numeric_literal(value: u64) -> Option<Rc<Self>> {
+        Some(Rc::new(Node::NumericLiteral(value)))
+    }
+}
