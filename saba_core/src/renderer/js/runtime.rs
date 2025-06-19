4b5dfce83fea17081cb7e0aaf2a41fc68740d7d3
watahiki606 <v10acdict@gmail.com>
2025-06-20 07:48:11 +0900

    ASTを実行する

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,8 +1,15 @@
+use crate::renderer::js::ast::Program;
 #[derive(Debug, Clone)]
 pub struct JsRuntime {}
 
 impl JsRuntime {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn execute(&mut self, program: &Program) {
+        for node in program.body() {
+            self.eval(&Some(node.clone()));
+        }
     }
 }
