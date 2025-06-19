cec227295001a82bfed18e98190e240483be5d50
watahiki606 <v10acdict@gmail.com>
2025-06-20 08:00:52 +0900

    evalの実装

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,4 +1,7 @@
+use crate::renderer::js::ast::Node;
 use crate::renderer::js::ast::Program;
+use alloc::rc::Rc;
+use core::borrow::Borrow;
 #[derive(Debug, Clone)]
 pub struct JsRuntime {}
 
@@ -10,6 +13,52 @@ impl JsRuntime {
     pub fn execute(&mut self, program: &Program) {
         for node in program.body() {
             self.eval(&Some(node.clone()));
+        }
+    }
+
+    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+        let node = match node {
+            Some(n) => n,
+            None => return None,
+        };
+
+        match node.borrow() {
+            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::AdditiveExpression {
+                operator,
+                left,
+                right,
+            } => {
+                let left_value = match self.eval(&left) {
+                    Some(value) => value,
+                    None => return None,
+                };
+                let right_value = match self.eval(&right) {
+                    Some(value) => value,
+                    None => return None,
+                };
+
+                if operator == &'+' {
+                    Some(left_value + right_value)
+                } else if operator == &'-' {
+                    Some(left_value - right_value)
+                } else {
+                    None
+                }
+            }
+            Node::AssignmentExpression {
+                operator: _,
+                left: _,
+                right: _,
+            } => {
+                // 後ほど実装
+                None
+            }
+            Node::MemberExpression { object, property } => {
+                // 後ほど実装
+                None
+            }
+            Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
         }
     }
 }
