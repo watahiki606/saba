e2409a2b81ead23023d35a9c53c269ae2a1ac34d
watahiki606 <v10acdict@gmail.com>
2025-06-22 11:40:41 +0900

    足し算のテスト

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -111,4 +111,21 @@ mod tests {
             i += 1;
         }
     }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(3))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
 }
